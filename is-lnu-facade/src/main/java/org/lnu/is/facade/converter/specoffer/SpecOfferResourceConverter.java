9adc39b84e78adcbf29c120c835dd52ccf5abb4b
Ivan Ursul <i.ursul@optigra.org>
2014-10-29 20:46:59 +0200

    Added rest of spec offer logic

--- is-lnu-facade/src/main/java/org/lnu/is/facade/converter/specoffer/SpecOfferResourceConverter.java
+++ is-lnu-facade/src/main/java/org/lnu/is/facade/converter/specoffer/SpecOfferResourceConverter.java
@@ -1,6 +1,13 @@
 package org.lnu.is.facade.converter.specoffer;
 
+import java.util.Date;
+
+import org.lnu.is.domain.department.Department;
+import org.lnu.is.domain.eduformtype.EduFormType;
+import org.lnu.is.domain.specialty.Specialty;
 import org.lnu.is.domain.specoffer.SpecOffer;
+import org.lnu.is.domain.specoffertype.SpecOfferType;
+import org.lnu.is.domain.timeperiod.TimePeriod;
 import org.lnu.is.facade.converter.AbstractConverter;
 import org.lnu.is.facade.resources.specoffer.SpecOfferResource;
 import org.springframework.stereotype.Component;
@@ -15,6 +22,42 @@ public class SpecOfferResourceConverter extends AbstractConverter<SpecOfferResou
 
 	@Override
 	public SpecOffer convert(final SpecOfferResource source, final SpecOffer target) {
+		
+		Department department = new Department();
+		department.setId(source.getDepartmentId());
+		
+		EduFormType eduFormType = new EduFormType();
+		eduFormType.setId(source.getEduFormTypeId());
+		
+		Specialty specialty = new Specialty();
+		specialty.setId(source.getSpecialtyId());
+
+		SpecOfferType specOfferType = new SpecOfferType();
+		specOfferType.setId(source.getSpecofferTypeId());
+
+		TimePeriod timePeriod = new TimePeriod();
+		timePeriod.setId(source.getTimePeriodId());
+
+		target.setActual(source.getActual());
+		target.setBegDate(source.getBegDate());
+		target.setDepartment(department);
+		target.setDocNum(source.getDocNum());
+		target.setDocSeries(source.getDocSeries());
+		target.setEduFormType(eduFormType);
+		target.setEndDate(source.getEndDate());
+		target.setId(source.getId());
+		target.setLicCount(source.getLicCount());
+		target.setNote(source.getNote());
+		target.setStatus(source.getStatus());
+		target.setSpecialty(specialty);
+		target.setSpecOfferType(specOfferType);
+		target.setStateCount(source.getStateCount());
+		target.setTimePeriod(timePeriod);
+		target.setUapp(source.getUapp());
+		target.setUid(source.getUid());
+		target.setUtid(source.getUtid());
+		target.setUpdateDate(new Date());
+		
 		return target;
 	}
 
--- is-lnu-facade/src/test/java/org/lnu/is/facade/converter/specoffer/SpecOfferResourceConverterTest.java
+++ is-lnu-facade/src/test/java/org/lnu/is/facade/converter/specoffer/SpecOfferResourceConverterTest.java
@@ -5,7 +5,13 @@ import static org.junit.Assert.assertEquals;
 import java.util.Date;
 
 import org.junit.Test;
+import org.lnu.is.domain.common.RowStatus;
+import org.lnu.is.domain.department.Department;
+import org.lnu.is.domain.eduformtype.EduFormType;
+import org.lnu.is.domain.specialty.Specialty;
 import org.lnu.is.domain.specoffer.SpecOffer;
+import org.lnu.is.domain.specoffertype.SpecOfferType;
+import org.lnu.is.domain.timeperiod.TimePeriod;
 import org.lnu.is.facade.resources.specoffer.SpecOfferResource;
 
 public class SpecOfferResourceConverterTest {
@@ -15,26 +21,93 @@ public class SpecOfferResourceConverterTest {
 	@Test
 	public void testConvert() throws Exception {
 		// Given
-    	String note = "Note";
-    	Integer licCount = 24;
-    	Long eduFormTypeId = 1L;
-    	String docNum = "docNum";
-    	Long departmentId = 23L;
-    	
-    	SpecOfferResource source = new SpecOfferResource();
-    	source.setBegDate(new Date());
-    	source.setEndDate(new Date());
+		Integer actualRow = 1;
+		Date begDate = new Date();
+		Department department = new Department();
+		Long departmentId = 1L;
+		department.setId(departmentId);
+		String docNum = "doc num";
+		String docSeries = "doc series";
+		Date endDate = new Date();
+		
+		Long eduFormTypeId = 2L;
+		EduFormType eduFormType = new EduFormType();
+		eduFormType.setId(eduFormTypeId);
+
+		Long id = 1L;
+		Integer licCount = 223;
+		String note = "soem notes";
+
+		Long parentId = 45L;
+		SpecOffer parent = new SpecOffer();
+		parent.setId(parentId);
+		
+		Long specialtyId = 4L;
+		Specialty specialty = new Specialty();
+		specialty.setId(specialtyId);
+
+		Long specOfferTypeId = 5L;
+		SpecOfferType specOfferType = new SpecOfferType();
+		specOfferType.setId(specOfferTypeId);
+		
+		Integer stateCount = 34;
+		RowStatus status = RowStatus.OK;
+		String uapp = "uap";
+		Long uid = 124L;
+		
+		Long timePeriodId = 5L;
+		TimePeriod timePeriod = new TimePeriod();
+		timePeriod.setId(timePeriodId);
+		
+		Date updateDate = new Date();
+		String utid = "utid";
+		
+		SpecOffer expected = new SpecOffer();
+		expected.setActual(actualRow);
+		expected.setBegDate(begDate);
+		expected.setDepartment(department);
+		expected.setDocNum(docNum);
+		expected.setDocSeries(docSeries);
+		expected.setEduFormType(eduFormType);
+		expected.setEndDate(endDate);
+		expected.setId(id);
+		expected.setLicCount(licCount);
+		expected.setNote(note);
+		expected.setParent(parent);
+		expected.setSpecialty(specialty);
+		expected.setSpecOfferType(specOfferType);
+		expected.setStateCount(stateCount);
+		expected.setStatus(status);
+		expected.setTimePeriod(timePeriod);
+		expected.setUapp(uapp);
+		expected.setUid(uid);
+		expected.setUpdateDate(updateDate);
+		expected.setUtid(utid);
+
+		SpecOfferResource source = new SpecOfferResource();
+		source.setActual(actualRow);
+		source.setBegDate(begDate);
 		source.setDepartmentId(departmentId);
 		source.setDocNum(docNum);
+		source.setDocSeries(docSeries);
 		source.setEduFormTypeId(eduFormTypeId);
+		source.setEndDate(endDate);
+		source.setId(id);
 		source.setLicCount(licCount);
 		source.setNote(note);
-
-		SpecOffer expected = new SpecOffer();
+		source.setSpecialtyId(specialtyId);
+		source.setSpecofferTypeId(specOfferTypeId);
+		source.setStateCount(stateCount);
+		source.setStatus(status);
+		source.setTimePeriodId(timePeriodId);
+		source.setUapp(uapp);
+		source.setUid(uid);
+		source.setUtid(utid);
 		
 		// When
 		SpecOffer actual = unit.convert(source);
-
+		expected.setUpdateDate(actual.getUpdateDate());
+		
 		// Then
 		assertEquals(expected, actual);
 	}
