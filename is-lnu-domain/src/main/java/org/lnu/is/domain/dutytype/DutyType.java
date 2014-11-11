27894004dda6229d1948b26a5b9738fc58e4f6f3
Ілля Мазуряк <illay1994@gmail.com>
2014-11-11 16:13:50 +0200

    add table and model dutytype and orderduty

---
+++ is-lnu-domain/src/main/java/org/lnu/is/domain/dutytype/DutyType.java
@@ -0,0 +1,82 @@
+package org.lnu.is.domain.dutytype;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.lnu.is.domain.Model;
+
+@Entity
+@Table(name = "q_rf_dutytype")
+public class DutyType extends Model {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "abbrname")
+	private String abbrname;
+	
+	@Column(name = "name")
+	private String name;
+
+	public String getAbbrname() {
+		return abbrname;
+	}
+
+	public void setAbbrname(final String abbrname) {
+		this.abbrname = abbrname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((abbrname == null) ? 0 : abbrname.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		DutyType other = (DutyType) obj;
+		if (abbrname == null) {
+			if (other.abbrname != null) {
+				return false;
+			}
+		} else if (!abbrname.equals(other.abbrname)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DutyType [abbrname=" + abbrname + ", name=" + name + "]";
+	}
+	
+	
+}
