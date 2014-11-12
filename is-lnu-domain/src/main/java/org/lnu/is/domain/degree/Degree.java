7d3778f3cb05e9df256c05eef5f2bb1b1f6e6c3f
kushnir_yura <kushnir.yb@gmail.com>
2014-11-12 14:16:57 +0200

    Added 12 models.

---
+++ is-lnu-domain/src/main/java/org/lnu/is/domain/degree/Degree.java
@@ -0,0 +1,46 @@
+package org.lnu.is.domain.degree;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.lnu.is.domain.Model;
+import org.lnu.is.domain.degreetype.DegreeType;
+import org.lnu.is.domain.person.Person;
+
+/**
+ * Degree entity.
+ * 
+ * @author kushnir
+ *
+ */
+@Table
+@Entity(name = "q_ob_degree")
+public class Degree extends Model {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "person_id")
+	private Person person;
+
+	@ManyToOne
+	@JoinColumn(name = "degreetype_id")
+	private DegreeType degreeType;
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(final Person person) {
+		this.person = person;
+	}
+
+	public DegreeType getDegreeType() {
+		return degreeType;
+	}
+
+	public void setDegreeType(final DegreeType degreeType) {
+		this.degreeType = degreeType;
+	}
+}
---
+++ is-lnu-domain/src/main/java/org/lnu/is/domain/orderemployeetimesheet/OrderEmployeeTimeSheet.java
@@ -0,0 +1,170 @@
+package org.lnu.is.domain.orderemployeetimesheet;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.lnu.is.domain.Model;
+import org.lnu.is.domain.employee.Employee;
+import org.lnu.is.domain.order.Order;
+import org.lnu.is.domain.timesheettype.TimeSheetType;
+
+/**
+ * Order Employee Time Sheet entity.
+ * 
+ * @author kushnir
+ *
+ */
+@Entity
+@Table(name = "q_dt_orderemployeetimesheet")
+public class OrderEmployeeTimeSheet extends Model {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne
+	@JoinColumn(name = "employee_id")
+	private Employee employee;
+
+	@ManyToOne
+	@JoinColumn(name = "timesheettype_id")
+	private TimeSheetType timeSheetType;
+
+	@Column(name = "dayamount")
+	private Date dayAmount;
+
+	@Column(name = "houramount")
+	private Date hourAmount;
+
+	@Column(name = "enddate")
+	private Date endDate;
+
+	@Column(name = "begdate")
+	private Date begDate;
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(final Order order) {
+		this.order = order;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(final Employee employee) {
+		this.employee = employee;
+	}
+
+	public TimeSheetType getTimeSheetType() {
+		return timeSheetType;
+	}
+
+	public void setTimeSheetType(final TimeSheetType timeSheetType) {
+		this.timeSheetType = timeSheetType;
+	}
+
+	public Date getDayAmount() {
+		return dayAmount;
+	}
+
+	public void setDayAmount(final Date dayAmount) {
+		this.dayAmount = dayAmount;
+	}
+
+	public Date getHourAmount() {
+		return hourAmount;
+	}
+
+	public void setHourAmount(final Date hourAmount) {
+		this.hourAmount = hourAmount;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(final Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getBegDate() {
+		return begDate;
+	}
+
+	public void setBegDate(final Date begDate) {
+		this.begDate = begDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((begDate == null) ? 0 : begDate.hashCode());
+		result = prime * result
+				+ ((dayAmount == null) ? 0 : dayAmount.hashCode());
+		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+		result = prime * result
+				+ ((hourAmount == null) ? 0 : hourAmount.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		OrderEmployeeTimeSheet other = (OrderEmployeeTimeSheet) obj;
+		if (begDate == null) {
+			if (other.begDate != null) {
+				return false;
+			}
+		} else if (!begDate.equals(other.begDate)) {
+			return false;
+		}
+		if (dayAmount == null) {
+			if (other.dayAmount != null) {
+				return false;
+			}
+		} else if (!dayAmount.equals(other.dayAmount)) {
+			return false;
+		}
+		if (endDate == null) {
+			if (other.endDate != null) {
+				return false;
+			}
+		} else if (!endDate.equals(other.endDate)) {
+			return false;
+		}
+		if (hourAmount == null) {
+			if (other.hourAmount != null) {
+				return false;
+			}
+		} else if (!hourAmount.equals(other.hourAmount)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderEmployeeTimeSheet [dayAmount=" + dayAmount
+				+ ", hourAmount=" + hourAmount + ", endDate=" + endDate
+				+ ", begDate=" + begDate + "]";
+	}
+
+}
