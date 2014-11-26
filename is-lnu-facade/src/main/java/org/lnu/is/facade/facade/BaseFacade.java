1f97edfdb5aaca8e92a5e306f4e4a7d61e2c7d75
Ivan Ursul <ivan.ursul@optigra-soft.com>
2014-11-26 18:21:49 +0200

    Added base facade skeleton

--- is-lnu-facade/src/main/java/org/lnu/is/facade/facade/BaseFacade.java
+++ is-lnu-facade/src/main/java/org/lnu/is/facade/facade/BaseFacade.java
@@ -16,7 +16,9 @@ import org.lnu.is.pagination.PagedSearch;
  * @param <ENTITY> entity.
  */
 public abstract class BaseFacade<RESOURCE, ENTITY> implements Facade<RESOURCE, ENTITY> {
-
+//	public abstract class BaseFacade<RESOURCE, ENTITY, SERVICE> implements Facade<RESOURCE, ENTITY> {
+//	private static final Logger LOG = LoggerFactory.getLogger(BaseFacade.class);
+	
 	@Resource(name = "insertConverter")
 	protected Converter<RESOURCE, ENTITY> insertConverter;
 	
@@ -28,5 +30,49 @@ public abstract class BaseFacade<RESOURCE, ENTITY> implements Facade<RESOURCE, E
 
 	@Resource(name = "pagedSearchConverter")
 	protected Converter<PagedResult<ENTITY>, PagedResultResource<RESOURCE>> pagedResultConverter;
+/*	
+	protected Converter<RESOURCE, ENTITY> resourceConverter;
 
+	protected Converter<ENTITY, RESOURCE> entityConverter;
+	
+	protected SERVICE service;
+
+	protected abstract PagedResult<ENTITY> getPagedResult(PagedSearch<ENTITY> request);
+	
+	protected abstract String getUriForPagedResult(PagedRequest<RESOURCE> request);
+	
+	public PagedResultResource<RESOURCE> getResources(final PagedRequest<RESOURCE> request) {
+		LOG.info("Getting paged result resource for {0}: {1}", request.getResource().getClass().getSimpleName(), request);
+
+		PagedSearch<ENTITY> pagedSearch = pagedRequestConverter.convert(request);
+		pagedSearch.setEntity(resourceConverter.convert(request.getResource()));
+
+		PagedResult<ENTITY> pagedResult = getPagedResult(pagedSearch);
+
+		List<RESOURCE> resources = entityConverter.convertAll(pagedResult.getEntities());
+
+		String uri = getUriForPagedResult(request);
+		PagedResultResource<RESOURCE> pagedResultResource = new PagedResultResource<>(uri);
+		pagedResultResource.setResources(resources);
+		pagedResultConverter.convert(pagedResult, pagedResultResource);
+
+		return pagedResultResource;
+	}
+
+	@Required
+	public void setResourceConverter(final Converter<RESOURCE, ENTITY> resourceConverter) {
+		this.resourceConverter = resourceConverter;
+	}
+
+	@Required
+	public void setEntityConverter(final Converter<ENTITY, RESOURCE> entityConverter) {
+		this.entityConverter = entityConverter;
+	}
+
+	@Required
+	public void setService(final SERVICE service) {
+		this.service = service;
+	}
+*/
+	
 }
