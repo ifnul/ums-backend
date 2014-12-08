9722c8dc83854e24bd714593a3aec8dc6c67b983
Ivan Ursul <ivan.ursul@optigra-soft.com>
2014-12-08 12:13:29 +0200

    patch for lnu

--- is-lnu-integration/src/test/scala/org/lnu/is/integration/person/address/PersonAddressIntegrationTest.scala
+++ is-lnu-integration/src/test/scala/org/lnu/is/integration/person/address/PersonAddressIntegrationTest.scala
@@ -1,9 +1,7 @@
-package org.lnu.is.integration
+package org.lnu.is.integration.person.address
 
 import java.util.UUID
-
 import scala.concurrent.duration.DurationInt
-
 import io.gatling.core.Predef.checkBuilder2Check
 import io.gatling.core.Predef.findCheckBuilder2ValidatorCheckBuilder
 import io.gatling.core.Predef.scenario
@@ -16,47 +14,46 @@ import io.gatling.http.Predef.http
 import io.gatling.http.Predef.jsonPath
 import io.gatling.http.Predef.status
 import io.gatling.http.request.builder.AbstractHttpRequestBuilder.toActionBuilder
+import org.lnu.is.integration.PersonIntegrationTest
 
-object PersonIntegrationTest {
-
+object PersonAddressIntegrationTest {
+	
   val scn = scenario("Manage Persons")
     .exec(session => {
       session
       	.set("idnum", UUID.randomUUID())
       	.set("other_value_example", "value")
     })
+    // Firstly, create person
     .exec(http("Post Person")
 		.post("/persons")
 		.header("Content-Type", "application/json")
 		.body(ELFileBody("data/person/post.json"))
 		.asJSON
 		.check(status.is(201))
-		.check(jsonPath("$.id").find.saveAs("identifier")))
-    .pause(500 milliseconds, 2 seconds)
-    .exec(
-      http("Get Person")
-        .get("/persons/${identifier}")
-        .check(status.is(200)))
-    .exec(
-      http("Update Person")
-        .put("/persons/${identifier}")
+		.check(jsonPath("$.id").find.saveAs("personId")))
+    // Then create address for that person
+	.exec(http("Post address")
+	    .post("/persons/${personId}/addresses")
+	    .header("Content-Type", "application/json")
+	    .body(ELFileBody("data/person/address/post.json"))
+	    .asJSON
+	    .check(status.is(201))
+	    .check(jsonPath("$.id").find.saveAs("personAddressId"))
+    )
+	// Get request
+    // Put request
+    // Get request to verify updated address
+    // Delete request for address
+    .exec(http("Delete address")
+        .delete("/persons/${personId}/addresses/${personAddressId}")
         .header("Content-Type", "application/json")
-        .body(ELFileBody("data/person/put.json"))
-        .asJSON
-        .check(status.is(200)))
-    .exec(
-      http("Get Person")
-        .get("/persons/${identifier}")
-        .check(status.is(200))
-        .check(jsonPath("$.name").find.is("name1")))
-    .exec(http("Delete Person")
-		.delete("/persons/${identifier}")
+        .check(status.is(204))
+    )
+    // Delete person
+	.exec(http("Delete Person")
+		.delete("/persons/${personId}")
 		.check(status.is(204))
     )
-    .exec(http("Get Person")
-		.get("/persons/${identifier}")
-    	.check(status.is(404))
-    )
       
-
 }
