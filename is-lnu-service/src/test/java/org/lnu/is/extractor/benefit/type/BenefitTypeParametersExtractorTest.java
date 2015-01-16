e3d066fd8835242863483d2ac9b8976a4c961d6d
Ivan Ursul <ivan.ursul@optigra-soft.com>
2015-01-16 11:44:19 +0200

    Added Department and benefit parameter extractor tests

--- is-lnu-service/src/test/java/org/lnu/is/extractor/benefit/type/BenefitTypeParametersExtractorTest.java
+++ is-lnu-service/src/test/java/org/lnu/is/extractor/benefit/type/BenefitTypeParametersExtractorTest.java
@@ -2,17 +2,48 @@ package org.lnu.is.extractor.benefit.type;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
+import org.lnu.is.domain.benefit.BenefitType;
 
 public class BenefitTypeParametersExtractorTest {
 
+	private BenefitTypeParametersExtractor unit = new BenefitTypeParametersExtractor();
+	
 	@Test
 	public void testGetParameters() throws Exception {
 		// Given
+		String name = "name1";
+		Integer priority = 5;
+		
+		BenefitType entity = new BenefitType();
+		entity.setName(name);
+		entity.setPriority(priority);
 
+		Map<String, Object> expected = new HashMap<String, Object>();
+		expected.put("name", name);
+		expected.put("priority", priority);
+		
 		// When
+		Map<String, Object> actual = unit.getParameters(entity);
 
 		// Then
-		assertEquals(true, true);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testGetParametersWithDefaultEntity() throws Exception {
+		// Given
+		BenefitType entity = new BenefitType();
+		
+		Map<String, Object> expected = Collections.emptyMap();
+		// When
+		Map<String, Object> actual = unit.getParameters(entity);
+
+		// Then
+		assertEquals(expected, actual);
 	}
 }
--- is-lnu-service/src/test/java/org/lnu/is/extractor/department/DepartmentParametersExtractorTest.java
+++ is-lnu-service/src/test/java/org/lnu/is/extractor/department/DepartmentParametersExtractorTest.java
@@ -1,18 +1,112 @@
 package org.lnu.is.extractor.department;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lnu.is.dao.dao.Dao;
+import org.lnu.is.domain.department.Department;
+import org.lnu.is.domain.department.DepartmentType;
+import org.lnu.is.domain.order.Order;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DepartmentParametersExtractorTest {
 
+	@Mock
+	private Dao<Department, Long> departmentDao;
+	
+	@Mock
+	private Dao<DepartmentType, Long> departmentTypeDao;
+	
+	@Mock
+	private Dao<Order, Long> orderDao;
+	
+	@InjectMocks
+	private DepartmentParametersExtractor unit;
+	
 	@Test
 	public void testGetParameters() throws Exception {
 		// Given
+		Long parentId = 1L;
+		Department parent = new Department();
+		parent.setId(parentId);
+		
+		Long departmentTypeId = 2L;
+		DepartmentType departmentType = new DepartmentType();
+		departmentType.setId(departmentTypeId);
 
+		Long orderId = 3L;
+		Order order = new Order();
+		order.setId(orderId);
+		
+		String abbrName = "abbr name";
+		String name = "name1";
+		String manager = "manager1";
+		String phone = "542534534";
+		String email = "email@email.emao;";
+		Date begDate = new Date();
+		Date endDate = new Date();
+
+		Department entity = new Department();
+		entity.setParent(parent);
+		entity.setDepartmentType(departmentType);
+		entity.setOrder(order);
+		
+		entity.setAbbrName(abbrName);
+		entity.setName(name);
+		entity.setManager(manager);
+		entity.setPhone(phone);
+		entity.setEmail(email);
+		entity.setBegDate(begDate);
+		entity.setEndDate(endDate);
+		
+		Map<String, Object> expected = new HashMap<String, Object>();
+		expected.put("parent", parent);
+		expected.put("departmentType", departmentType);
+		expected.put("order", order);
+		expected.put("abbrName", abbrName);
+		expected.put("name", name);
+		expected.put("manager", manager);
+		expected.put("phone", phone);
+		expected.put("email", email);
+		expected.put("begDate", begDate);
+		expected.put("endDate", endDate);
+		
 		// When
+		when(departmentDao.getEntityById(anyLong())).thenReturn(parent);
+		when(departmentTypeDao.getEntityById(anyLong())).thenReturn(departmentType);
+		when(orderDao.getEntityById(anyLong())).thenReturn(order);
+		
+		Map<String, Object> actual = unit.getParameters(entity);
 
 		// Then
-		assertEquals(true, true);
+		verify(departmentDao).getEntityById(parentId);
+		verify(departmentTypeDao).getEntityById(departmentTypeId);
+		verify(orderDao).getEntityById(orderId);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testGetParametersWithDefaultEntity() throws Exception {
+		// Given
+		Department entity = new Department();
+		
+		Map<String, Object> expected = Collections.emptyMap();
+		// When
+		Map<String, Object> actual = unit.getParameters(entity);
+
+		// Then
+		assertEquals(expected, actual);
 	}
 }
