e3d043dec407a61be35e584e5fa5bf68d0789af3
Ivan Ursul <i.ursul@optigra.org>
2015-01-17 12:29:06 +0200

    Added specoffer type converter test cases

---
+++ is-lnu-converter/src/test/java/org/lnu/is/converter/specoffer/type/SpecOfferTypeConverterTest.java
@@ -0,0 +1,43 @@
+package org.lnu.is.converter.specoffer.type;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.lnu.is.domain.specialty.SpecialtyType;
+import org.lnu.is.domain.specoffer.SpecOfferType;
+import org.lnu.is.resource.specoffer.type.SpecOfferTypeResource;
+
+public class SpecOfferTypeConverterTest {
+
+	private SpecOfferTypeConverter unit = new SpecOfferTypeConverter();
+
+	@Test
+	public void testConvert() throws Exception {
+		// Given
+		String name = "first blood";
+		String abbrName = "fb";
+
+		Long id = 1L;
+		Long specialtyTypeId = 2L;
+		SpecialtyType specialType = new SpecialtyType();
+		specialType.setId(specialtyTypeId);
+		
+		SpecOfferType source = new SpecOfferType();
+		source.setName(name);
+		source.setAbbrName(abbrName);
+		source.setId(id);
+		source.setSpecialtyType(specialType);
+
+		SpecOfferTypeResource expected = new SpecOfferTypeResource();
+		expected.setName(name);
+		expected.setAbbrName(abbrName);
+		expected.setSpecialtyTypeId(specialtyTypeId);
+		expected.setId(id);
+		
+		// When
+		SpecOfferTypeResource actual = unit.convert(source);
+		
+		// Then
+		assertEquals(expected, actual);
+	}
+}
---
+++ is-lnu-converter/src/test/java/org/lnu/is/converter/specoffer/type/SpecOfferTypeResourceConverterTest.java
@@ -0,0 +1,43 @@
+package org.lnu.is.converter.specoffer.type;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.lnu.is.domain.specialty.SpecialtyType;
+import org.lnu.is.domain.specoffer.SpecOfferType;
+import org.lnu.is.resource.specoffer.type.SpecOfferTypeResource;
+
+public class SpecOfferTypeResourceConverterTest {
+
+	private SpecOfferTypeResourceConverter unit = new SpecOfferTypeResourceConverter();
+
+	@Test
+	public void testConvert() throws Exception {
+		// Given
+		String name = "first blood";
+		String abbrName = "fb";
+
+		Long id = 1L;
+		Long specialtyTypeId = 2L;
+		SpecialtyType specialType = new SpecialtyType();
+		specialType.setId(specialtyTypeId);
+		
+		SpecOfferType expected = new SpecOfferType();
+		expected.setName(name);
+		expected.setAbbrName(abbrName);
+		expected.setId(id);
+		expected.setSpecialtyType(specialType);
+
+		SpecOfferTypeResource source = new SpecOfferTypeResource();
+		source.setName(name);
+		source.setAbbrName(abbrName);
+		source.setSpecialtyTypeId(specialtyTypeId);
+		source.setId(id);
+		
+		// When
+		SpecOfferType actual = unit.convert(source);
+		
+		// Then
+		assertEquals(expected, actual);
+	}
+}
