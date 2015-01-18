f700ae53cdabeb208121e001a16a964c501ff51b
Ivan Ursul <i.ursul@optigra.org>
2015-01-18 17:08:46 +0200

    Added subject controller test for get by id method

--- is-lnu-rest-api/src/test/java/org/lnu/is/web/rest/controller/subject/SubjectControllerTest.java
+++ is-lnu-rest-api/src/test/java/org/lnu/is/web/rest/controller/subject/SubjectControllerTest.java
@@ -1,5 +1,6 @@
 package org.lnu.is.web.rest.controller.subject;
 
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -72,4 +73,24 @@ public class SubjectControllerTest extends AbstractControllerTest {
 		verify(facade).getResources(request);
 	}
 
+	@Test
+	public void testGetResource() throws Exception {
+		// Given
+		Long id = 1L;
+		String name = "all difficult";
+		SubjectResource expected = new SubjectResource();
+		expected.setName(name);
+		expected.setId(id);
+		
+		// When
+		when(facade.getResource(anyLong())).thenReturn(expected);
+		String response = getJson(expected, false);
+
+		// Then
+		mockMvc.perform(get("/subjects/{id}", id))
+			.andExpect(status().isOk())
+			.andExpect(content().string(response));
+		
+		verify(facade).getResource(id);
+	}
 }
