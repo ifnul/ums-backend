d6aa5cfbb7e133a487d8fa95d2e434613ca9047f
Ivan Ursul <i.ursul@optigra.org>
2015-01-18 17:22:04 +0200

    Added test for webservicemethodcountprocessor

---
+++ is-lnu-rest-api/src/test/java/org/lnu/is/web/rest/processor/api/WebServiceMethodCountBeanFactoryPostProcessorTest.java
@@ -0,0 +1,63 @@
+package org.lnu.is.web.rest.processor.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lnu.is.domain.department.DepartmentType;
+import org.lnu.is.web.rest.controller.department.DepartmentController;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WebServiceMethodCountBeanFactoryPostProcessorTest {
+
+	@Mock
+	private ConfigurableListableBeanFactory beanFactory;
+
+	private WebServiceMethodCountBeanFactoryPostProcessor unit = new WebServiceMethodCountBeanFactoryPostProcessor();
+	
+	@Test
+	public void testpostProcessBeanFactory() throws Exception {
+		// Given
+		String[] beanDefinitionNames = {"bean1", "bean2"};
+
+		// When
+		when(beanFactory.getBeanDefinitionNames()).thenReturn(beanDefinitionNames);
+		
+		when(beanFactory.getType("bean1")).thenAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(final InvocationOnMock invocationOnMock) throws Throwable {
+                return DepartmentController.class;
+            }
+        });
+
+		when(beanFactory.getType("bean2")).thenAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(final InvocationOnMock invocationOnMock) throws Throwable {
+				return DepartmentType.class;
+			}
+		});
+		
+		unit.postProcessBeanFactory(beanFactory);
+
+		// Then
+		verify(beanFactory).getBeanDefinitionNames();
+		assertEquals(4, unit.getMethodsCount().size());
+	}
+	
+	public static <T> Answer<T> createAnswer(final T value) {
+	    Answer<T> dummy = new Answer<T>() {
+	        @Override
+	        public T answer(final InvocationOnMock invocation) throws Throwable {
+	            return value;
+	        }
+	    };
+	    return dummy;
+	}
+}
