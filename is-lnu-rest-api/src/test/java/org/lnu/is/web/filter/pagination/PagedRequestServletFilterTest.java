a9f897d898a3060c85244adf03d86d963c75b14d
Ivan Ursul <i.ursul@optigra.org>
2015-01-18 15:47:25 +0200

    Imroved PagedRequestServletFilter

--- is-lnu-rest-api/src/test/java/org/lnu/is/web/filter/pagination/PagedRequestServletFilterTest.java
+++ is-lnu-rest-api/src/test/java/org/lnu/is/web/filter/pagination/PagedRequestServletFilterTest.java
@@ -2,6 +2,7 @@ package org.lnu.is.web.filter.pagination;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
@@ -9,6 +10,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,10 +21,15 @@ import org.junit.runner.RunWith;
 import org.lnu.is.web.exception.LimitOutOfRangeException;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PagedRequestServletFilterTest {
 
+	private static final String MAX_PAGINATION_COUNT = "max.pagination.count";
+	private static final Integer DEFAULT_PAGINATION_COUNT = 300;
+	
 	private PagedRequestServletFilter unit = new PagedRequestServletFilter();
 	
 	@Mock
@@ -33,8 +41,20 @@ public class PagedRequestServletFilterTest {
 	@Mock
 	private FilterChain chain;
 
-	private Integer maxPaginationCount = 400;
+	@Mock
+	private FilterConfig config;
 	
+	@Mock
+	private ServletContext context;
+
+	@Mock
+	private WebApplicationContext webApplicationContext;
+
+	@Mock
+	private Environment env;
+
+	private Integer maxPaginationCount = 400;
+
 	@Before
 	public void setup() {
 		unit.setMaxPaginationCount(maxPaginationCount);
@@ -103,5 +123,29 @@ public class PagedRequestServletFilterTest {
 		when(req.getParameter("limit")).thenReturn(limits[0]);
 		when(req.getParameterMap()).thenReturn(parameterMap);
 		unit.doFilter(req, response, chain);
+	}
+	
+	@Test
+	public void testDestroy() throws Exception {
+		unit.destroy();
+		verifyZeroInteractions(req);
+		verifyZeroInteractions(response);
+		verifyZeroInteractions(chain);
+	}
+	
+	@Test
+	public void testInit() throws Exception {
+		// When
+		when(config.getServletContext()).thenReturn(context);
+		when(context.getAttribute(WebApplicationContext.class.getName() + ".ROOT")).thenReturn(webApplicationContext);
+		when(webApplicationContext.getEnvironment()).thenReturn(env);
+		when(env.getProperty(MAX_PAGINATION_COUNT, Integer.class, DEFAULT_PAGINATION_COUNT)).thenReturn(maxPaginationCount);
+		unit.init(config);
+
+		// Then
+		verify(config).getServletContext();
+		verify(context).getAttribute(WebApplicationContext.class.getName() + ".ROOT");
+		verify(webApplicationContext).getEnvironment();
+		verify(env).getProperty(MAX_PAGINATION_COUNT, Integer.class, DEFAULT_PAGINATION_COUNT);
 	}
 }
