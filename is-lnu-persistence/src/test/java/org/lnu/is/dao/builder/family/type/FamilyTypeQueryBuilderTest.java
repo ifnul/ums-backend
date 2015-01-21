d9e91eaa7a0df9f990a3ecb1d3d54f03f4f2ffb6
Ivan Ursul <ivan.ursul@optigra-soft.com>
2015-01-21 18:39:27 +0200

    Aded couple of test cases, #7

--- is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/family/type/FamilyTypeQueryBuilderTest.java
+++ is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/family/type/FamilyTypeQueryBuilderTest.java
@@ -18,7 +18,6 @@ public class FamilyTypeQueryBuilderTest {
 		unit.setActive(active);
 		unit.setSecurity(security);
 	}
-	
 
 	@Test
 	public void testBuild() throws Exception {
@@ -26,6 +25,55 @@ public class FamilyTypeQueryBuilderTest {
 		FamilyType context = new FamilyType();
 		
 		String expectedQuery = "SELECT e FROM FamilyType e WHERE e.status=:status AND e.crtUserGroup IN (:userGroups) ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+
+	@Test
+	public void testBuildWithDisabledSecurityConstraints() throws Exception {
+		// Given
+		unit.setSecurity(false);
+		
+		FamilyType context = new FamilyType();
+		
+		String expectedQuery = "SELECT e FROM FamilyType e WHERE e.status=:status ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledStatusConstsints() throws Exception {
+		// Given
+		unit.setActive(false);
+		
+		FamilyType context = new FamilyType();
+		
+		String expectedQuery = "SELECT e FROM FamilyType e WHERE e.crtUserGroup IN (:userGroups) ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledDefaultConstraints() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		FamilyType context = new FamilyType();
+		
+		String expectedQuery = "SELECT e FROM FamilyType e ";
 		
 		// When
 		String actualQuery = unit.build(context);
@@ -49,6 +97,28 @@ public class FamilyTypeQueryBuilderTest {
 		// When
 		String actualQuery = unit.build(context);
 
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithParametersAndDIsabledDEfaultConstr() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		String abbrName = "AN";
+		String name = "fsdfsdfdsfsd";
+		
+		FamilyType context = new FamilyType();
+		context.setAbbrName(abbrName);
+		context.setName(name);
+		
+		String expectedQuery = "SELECT e FROM FamilyType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') ) ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
 		// Then
 		assertEquals(expectedQuery, actualQuery);
 	}
--- is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/gendertype/GenderTypeQueryBuilderTest.java
+++ is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/gendertype/GenderTypeQueryBuilderTest.java
@@ -18,7 +18,6 @@ public class GenderTypeQueryBuilderTest {
 		unit.setActive(active);
 		unit.setSecurity(security);
 	}
-	
 
 	@Test
 	public void testBuild() throws Exception {
@@ -26,6 +25,55 @@ public class GenderTypeQueryBuilderTest {
 		GenderType context = new GenderType();
 		
 		String expectedQuery = "SELECT e FROM GenderType e WHERE e.status=:status AND e.crtUserGroup IN (:userGroups) ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledSecurityCOnstraint() throws Exception {
+		// Given
+		unit.setSecurity(false);
+		
+		GenderType context = new GenderType();
+		
+		String expectedQuery = "SELECT e FROM GenderType e WHERE e.status=:status ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledStatusCosntraint() throws Exception {
+		// Given
+		unit.setActive(false);
+		
+		GenderType context = new GenderType();
+		
+		String expectedQuery = "SELECT e FROM GenderType e WHERE e.crtUserGroup IN (:userGroups) ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledDefaultConstraints() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		GenderType context = new GenderType();
+		
+		String expectedQuery = "SELECT e FROM GenderType e ";
 		
 		// When
 		String actualQuery = unit.build(context);
@@ -46,6 +94,25 @@ public class GenderTypeQueryBuilderTest {
 		// When
 		String actualQuery = unit.build(context);
 
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithParametersWithDisabledDefaultConstraints() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		String abbrName = "abbrname";
+		GenderType context = new GenderType();
+		context.setAbbrName(abbrName);
+		
+		String expectedQuery = "SELECT e FROM GenderType e WHERE ( e.abbrName LIKE CONCAT('%',:abbrName,'%') ) ";
+		
+		// When
+		String actualQuery = unit.build(context);
+		
 		// Then
 		assertEquals(expectedQuery, actualQuery);
 	}
