dd3a41dc9ff9300f3a0828004be99a63767de87a
Ivan Ursul <i.ursul@optigra.org>
2015-01-24 20:32:33 +0200

    Added additional test case for AdminUnitType Controller

--- is-lnu-rest-api/src/test/java/org/lnu/is/web/rest/controller/adminunit/type/AdminUnitTypeControllerTest.java
+++ is-lnu-rest-api/src/test/java/org/lnu/is/web/rest/controller/adminunit/type/AdminUnitTypeControllerTest.java
@@ -1,12 +1,14 @@
 package org.lnu.is.web.rest.controller.adminunit.type;
 
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -94,6 +96,29 @@ public class AdminUnitTypeControllerTest extends AbstractControllerTest {
 		mockMvc.perform(get("/adminunits/types/{id}", id))
 			.andExpect(status().isOk())
 			.andExpect(content().string(response));
+		
+		verify(facade).getResource(id);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void testGetResourceWithException() throws Exception {
+		// Given
+		Long id = 1L;
+		String name = "all difficult";
+		String abbrName = "ad";
+		AdminUnitTypeResource expected = new AdminUnitTypeResource();
+		expected.setName(name);
+		expected.setAbbrName(abbrName);
+		expected.setId(id);
+		
+		// When
+		doThrow(AccessDeniedException.class).when(facade).getResource(anyLong());
+		String response = getJson(expected, false);
+		
+		// Then
+		mockMvc.perform(get("/adminunits/types/{id}", id))
+		.andExpect(status().isOk())
+		.andExpect(content().string(response));
 		
 		verify(facade).getResource(id);
 	}
