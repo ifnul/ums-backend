d6a5e996459a66b0bdc1accbf8e807bf785841b3
Ivan Ursul <i.ursul@optigra.org>
2015-02-08 23:09:05 +0200

    Added maven sql plugin

---
+++ is-lnu-sql/src/main/resources/cleaning/clean.sql
@@ -0,0 +1,2 @@
+INSERT INTO q_rf_reason(abbrname, name, status, actual, note, crtuser, crtusergroup, update_date, create_date)
+    VALUES ('MSP', 'Maven Sql Plugin',  'ACTIVE', 1, 'a', 'ivanursul', 'developers', now(), now());
