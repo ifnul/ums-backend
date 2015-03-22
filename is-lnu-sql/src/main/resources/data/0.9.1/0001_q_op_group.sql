1cc72a463af96f63d7d484f844468bf7bebe1ca6
Illya Mazyriak <illay1994@gmail.com>
2015-03-22 16:52:38 +0200

    add specoffer data Vstup 2014

--- is-lnu-sql/src/main/resources/data/0.9.1/0001_q_op_group.sql
+++ is-lnu-sql/src/main/resources/data/0.9.1/0001_q_op_group.sql
@@ -1,3 +1,3 @@
 INSERT INTO q_op_group(title, description, id, status, actual, note, crtuser, crtusergroup, create_date, update_date ) VALUES 
-	('developers', 'Role for developers from Ivan Franko Lviv University', 1, 'ACTIVE', 1, null, 'illay', 'public', now(), now()), 
-	('public', 'Public group for all users', 2, 'ACTIVE', 1, null, 'illay', 'public', now(), now())
+	('developers', 'Role for developers from Ivan Franko Lviv University', 1, 'ACTIVE', 1, 'null', 'illay', 'public', now(), now()), 
+	('public', 'Public group for all users', 2, 'ACTIVE', 1, 'null', 'illay', 'public', now(), now())
--- is-lnu-sql/src/main/resources/data/0.9.1/0040_q_ob_timeperiod.sql
+++ is-lnu-sql/src/main/resources/data/0.9.1/0040_q_ob_timeperiod.sql
@@ -1,2 +1,10 @@
 INSERT INTO q_ob_timeperiod(timeperiodtype_id, name, numvalue, begdate, enddate, parent_id, uid, utid, uapp, status, actual, note, crtuser, crtusergroup, update_date, create_date ) VALUES 
-	(1, 'Всупна компанія 2014', 2014, '2014-07-11T00:00:00', '2014-07-31T23:59:59', null, 4, null, 'edbo', 'ACTIVE', 1, null, 'illay', 'public', null, now())
+	(4, '1 курс', 1, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(4, '2 курс', 2, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(4, '3 курс', 3, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(4, '4 курс', 4, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(4, '5 курс', 5, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(4, '6 курс', 6, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(4, '7 курс', 7, null, null, null, null, null, null, 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(1, 'Всупна компанія 2014', 2014, '2014-07-11T00:00:00', '2014-07-31T23:59:59', null, 4, null, 'edbo', 'ACTIVE', 1, null, 'illay', 'public', null, now()), 
+	(1, 'Всупна компанія 2015', 2014, '2015-07-01T00:00:00', '2015-07-31T23:59:59', null, 4, null, 'edbo', 'ACTIVE', 1, null, 'illay', 'public', null, now())
