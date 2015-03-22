06d23727c933921027e59b6776f7b56091ddcf25
Oleh Zanevych <Oleh.Zanevych@gmail.com>
2015-03-23 01:02:44 +0200

    Add tests with ORDER BY to BenefitQueryBuilderTest

--- is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/benefit/BenefitQueryBuilderTest.java
+++ is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/benefit/BenefitQueryBuilderTest.java
@@ -2,13 +2,17 @@ package org.lnu.is.dao.builder.benefit;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.lnu.is.domain.benefit.Benefit;
 import org.lnu.is.domain.benefit.BenefitType;
 import org.lnu.is.pagination.MultiplePagedSearch;
+import org.lnu.is.pagination.OrderBy;
+import org.lnu.is.pagination.OrderByType;
 
 public class BenefitQueryBuilderTest {
 
@@ -40,7 +44,33 @@ public class BenefitQueryBuilderTest {
 	}
 	
 	@Test
-	public void testBuildWithDisabledSecurityCOnstraint() throws Exception {
+	public void testBuildWithOrderBy() throws Exception {
+		// Given
+		Benefit context = new Benefit();
+		
+		OrderBy orderBy1 = new OrderBy("benefitType", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("parent", OrderByType.DESC);
+		OrderBy orderBy3 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy4 = new OrderBy("name", OrderByType.DESC);
+		OrderBy orderBy5 = new OrderBy("begdate", OrderByType.ASC);
+		OrderBy orderBy6 = new OrderBy("enddate", OrderByType.DESC);
+		OrderBy orderBy7 = new OrderBy("description", OrderByType.ASC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2, orderBy3, orderBy4, orderBy5, orderBy6, orderBy7);
+		
+		String expected = "SELECT e FROM Benefit e WHERE e.status=:status AND e.crtUserGroup IN (:userGroups) ORDER BY e.benefitType ASC, e.parent DESC, e.abbrName ASC, e.name DESC, e.begdate ASC, e.enddate DESC, e.description ASC";
+		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledSecurityConstraint() throws Exception {
 		// Given
 		unit.setSecurity(false);
 		
@@ -49,6 +79,34 @@ public class BenefitQueryBuilderTest {
 		String expected = "SELECT e FROM Benefit e WHERE e.status=:status ";
 		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledSecurityConstraintWithOrderBy() throws Exception {
+		// Given
+		unit.setSecurity(false);
+		
+		Benefit context = new Benefit();
+		
+		OrderBy orderBy1 = new OrderBy("benefitType", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("parent", OrderByType.DESC);
+		OrderBy orderBy3 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy4 = new OrderBy("name", OrderByType.DESC);
+		OrderBy orderBy5 = new OrderBy("begdate", OrderByType.ASC);
+		OrderBy orderBy6 = new OrderBy("enddate", OrderByType.DESC);
+		OrderBy orderBy7 = new OrderBy("description", OrderByType.ASC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2, orderBy3, orderBy4, orderBy5, orderBy6, orderBy7);
+		
+		String expected = "SELECT e FROM Benefit e WHERE e.status=:status ORDER BY e.benefitType ASC, e.parent DESC, e.abbrName ASC, e.name DESC, e.begdate ASC, e.enddate DESC, e.description ASC";
+		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -76,6 +134,34 @@ public class BenefitQueryBuilderTest {
 	}
 	
 	@Test
+	public void testBuildWithDisabledStatusConstraintWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		
+		Benefit context = new Benefit();
+		
+		OrderBy orderBy1 = new OrderBy("benefitType", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("parent", OrderByType.DESC);
+		OrderBy orderBy3 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy4 = new OrderBy("name", OrderByType.DESC);
+		OrderBy orderBy5 = new OrderBy("begdate", OrderByType.ASC);
+		OrderBy orderBy6 = new OrderBy("enddate", OrderByType.DESC);
+		OrderBy orderBy7 = new OrderBy("description", OrderByType.ASC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2, orderBy3, orderBy4, orderBy5, orderBy6, orderBy7);
+		
+		String expected = "SELECT e FROM Benefit e WHERE e.crtUserGroup IN (:userGroups) ORDER BY e.benefitType ASC, e.parent DESC, e.abbrName ASC, e.name DESC, e.begdate ASC, e.enddate DESC, e.description ASC";
+		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
 	public void testBuildWithDisabledDEfaultConstraints() throws Exception {
 		// Given
 		unit.setActive(false);
@@ -86,6 +172,35 @@ public class BenefitQueryBuilderTest {
 		String expected = "SELECT e FROM Benefit e ";
 		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledDEfaultConstraintsWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		Benefit context = new Benefit();
+		
+		OrderBy orderBy1 = new OrderBy("benefitType", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("parent", OrderByType.DESC);
+		OrderBy orderBy3 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy4 = new OrderBy("name", OrderByType.DESC);
+		OrderBy orderBy5 = new OrderBy("begdate", OrderByType.ASC);
+		OrderBy orderBy6 = new OrderBy("enddate", OrderByType.DESC);
+		OrderBy orderBy7 = new OrderBy("description", OrderByType.ASC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2, orderBy3, orderBy4, orderBy5, orderBy6, orderBy7);
+		
+		String expected = "SELECT e FROM Benefit e ORDER BY e.benefitType ASC, e.parent DESC, e.abbrName ASC, e.name DESC, e.begdate ASC, e.enddate DESC, e.description ASC";
+		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -115,10 +230,51 @@ public class BenefitQueryBuilderTest {
 		context.setEndDate(endDate);
 		context.setDescription(description);
 		
-		
 		String expected = "SELECT e FROM Benefit e WHERE ( e.benefitType = :benefitType OR e.parent = :parent OR e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') OR e.description LIKE CONCAT('%',:description,'%') OR e.begDate <= :begDate OR e.endDate >= :endDate ) AND e.status=:status AND e.crtUserGroup IN (:userGroups) ";
 		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testConvertWithParametersWithOrderBy() throws Exception {
+		// Given
+		BenefitType benefitType = new BenefitType();
+		Benefit parent = new Benefit();
+
+		String abbrName = "fsdsfds";
+		String name = "name1";
+		Date begDate = new Date();
+		Date endDate = new Date();
+		String description = "dsafds";
+
+		Benefit context = new Benefit();
+		context.setBenefitType(benefitType);
+		context.setParent(parent);
+		context.setAbbrName(abbrName);
+		context.setName(name);
+		context.setBegDate(begDate);
+		context.setEndDate(endDate);
+		context.setDescription(description);
+		
+		OrderBy orderBy1 = new OrderBy("benefitType", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("parent", OrderByType.DESC);
+		OrderBy orderBy3 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy4 = new OrderBy("name", OrderByType.DESC);
+		OrderBy orderBy5 = new OrderBy("begdate", OrderByType.ASC);
+		OrderBy orderBy6 = new OrderBy("enddate", OrderByType.DESC);
+		OrderBy orderBy7 = new OrderBy("description", OrderByType.ASC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2, orderBy3, orderBy4, orderBy5, orderBy6, orderBy7);
+		
+		String expected = "SELECT e FROM Benefit e WHERE ( e.benefitType = :benefitType OR e.parent = :parent OR e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') OR e.description LIKE CONCAT('%',:description,'%') OR e.begDate <= :begDate OR e.endDate >= :endDate ) AND e.status=:status AND e.crtUserGroup IN (:userGroups) ORDER BY e.benefitType ASC, e.parent DESC, e.abbrName ASC, e.name DESC, e.begdate ASC, e.enddate DESC, e.description ASC";
+		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -151,10 +307,54 @@ public class BenefitQueryBuilderTest {
 		context.setEndDate(endDate);
 		context.setDescription(description);
 		
-		
 		String expected = "SELECT e FROM Benefit e WHERE ( e.benefitType = :benefitType OR e.parent = :parent OR e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') OR e.description LIKE CONCAT('%',:description,'%') OR e.begDate <= :begDate OR e.endDate >= :endDate ) ";
 		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testConvertWithParametersAndDisabledDefaultConstraintsWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		BenefitType benefitType = new BenefitType();
+		Benefit parent = new Benefit();
+		
+		String abbrName = "fsdsfds";
+		String name = "name1";
+		Date begDate = new Date();
+		Date endDate = new Date();
+		String description = "dsafds";
+		
+		Benefit context = new Benefit();
+		context.setBenefitType(benefitType);
+		context.setParent(parent);
+		context.setAbbrName(abbrName);
+		context.setName(name);
+		context.setBegDate(begDate);
+		context.setEndDate(endDate);
+		context.setDescription(description);
+		
+		OrderBy orderBy1 = new OrderBy("benefitType", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("parent", OrderByType.DESC);
+		OrderBy orderBy3 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy4 = new OrderBy("name", OrderByType.DESC);
+		OrderBy orderBy5 = new OrderBy("begdate", OrderByType.ASC);
+		OrderBy orderBy6 = new OrderBy("enddate", OrderByType.DESC);
+		OrderBy orderBy7 = new OrderBy("description", OrderByType.ASC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2, orderBy3, orderBy4, orderBy5, orderBy6, orderBy7);
+		
+		String expected = "SELECT e FROM Benefit e WHERE ( e.benefitType = :benefitType OR e.parent = :parent OR e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') OR e.description LIKE CONCAT('%',:description,'%') OR e.begDate <= :begDate OR e.endDate >= :endDate ) ORDER BY e.benefitType ASC, e.parent DESC, e.abbrName ASC, e.name DESC, e.begdate ASC, e.enddate DESC, e.description ASC";
+		MultiplePagedSearch<Benefit> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
