8811e2cfd8a0b9d7cfbad1327634651ebe5c5b03
Oleh Zanevych <Oleh.Zanevych@gmail.com>
2015-03-23 10:54:54 +0200

    Add tests with ORDER BY to DegreeTypeQueryBuilderTest

--- is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/degree/type/DegreeTypeQueryBuilderTest.java
+++ is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/degree/type/DegreeTypeQueryBuilderTest.java
@@ -2,10 +2,15 @@ package org.lnu.is.dao.builder.degree.type;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.lnu.is.domain.degree.DegreeType;
 import org.lnu.is.pagination.MultiplePagedSearch;
+import org.lnu.is.pagination.OrderBy;
+import org.lnu.is.pagination.OrderByType;
 
 public class DegreeTypeQueryBuilderTest {
 
@@ -37,6 +42,27 @@ public class DegreeTypeQueryBuilderTest {
 	}
 	
 	@Test
+	public void testBuildWithOrderBy() throws Exception {
+		// Given
+		DegreeType context = new DegreeType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+
+		String expectedQuery = "SELECT e FROM DegreeType e WHERE e.status=:status AND e.crtUserGroup IN (:userGroups) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
 	public void testBuildWithDisabledSecurityConstraint() throws Exception {
 		// Given
 		unit.setSecurity(false);
@@ -55,14 +81,61 @@ public class DegreeTypeQueryBuilderTest {
 	}
 	
 	@Test
+	public void testBuildWithDisabledSecurityConstraintWithOrderBy() throws Exception {
+		// Given
+		unit.setSecurity(false);
+		
+		DegreeType context = new DegreeType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expectedQuery = "SELECT e FROM DegreeType e WHERE e.status=:status ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
 	public void testBuildWithDisabledStatusConstraint() throws Exception {
 		// Given
 		unit.setActive(false);
+		
 		DegreeType context = new DegreeType();
 		
 		String expectedQuery = "SELECT e FROM DegreeType e WHERE e.crtUserGroup IN (:userGroups) ";
 		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledStatusConstraintWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		
+		DegreeType context = new DegreeType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expectedQuery = "SELECT e FROM DegreeType e WHERE e.crtUserGroup IN (:userGroups) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -82,6 +155,30 @@ public class DegreeTypeQueryBuilderTest {
 		String expectedQuery = "SELECT e FROM DegreeType e ";
 		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledDefaultConstraintsWithOrderBy() throws Exception {
+		// Given
+		unit.setSecurity(false);
+		unit.setActive(false);
+		
+		DegreeType context = new DegreeType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expectedQuery = "SELECT e FROM DegreeType e ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -112,7 +209,33 @@ public class DegreeTypeQueryBuilderTest {
 	}
 	
 	@Test
-	public void testBuildWithParametersAndDisabledDEfaultConstraints() throws Exception {
+	public void testBuildWithParametersWithOrderBy() throws Exception {
+		// Given
+		String abbrName = "AN";
+		String name = "namdsfsd";
+		
+		DegreeType context = new DegreeType();
+		context.setAbbrName(abbrName);
+		context.setName(name);
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expectedQuery = "SELECT e FROM DegreeType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrname LIKE CONCAT('%',:abbrName,'%') ) AND e.status=:status AND e.crtUserGroup IN (:userGroups) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithParametersAndDisabledDefaultConstraints() throws Exception {
 		// Given
 		unit.setActive(false);
 		unit.setSecurity(false);
@@ -127,6 +250,35 @@ public class DegreeTypeQueryBuilderTest {
 		String expectedQuery = "SELECT e FROM DegreeType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrname LIKE CONCAT('%',:abbrName,'%') ) ";
 		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expectedQuery, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithParametersAndDisabledDefaultConstraintsWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		String abbrName = "AN";
+		String name = "namdsfsd";
+		
+		DegreeType context = new DegreeType();
+		context.setAbbrName(abbrName);
+		context.setName(name);
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expectedQuery = "SELECT e FROM DegreeType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrname LIKE CONCAT('%',:abbrName,'%') ) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<DegreeType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
