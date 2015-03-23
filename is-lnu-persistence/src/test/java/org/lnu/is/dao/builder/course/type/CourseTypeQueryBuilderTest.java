77a079b22bc947391702b2c4b3f1d84c3756efec
Oleh Zanevych <Oleh.Zanevych@gmail.com>
2015-03-23 10:04:34 +0200

    Add tests with ORDER BY to CourseTypeQueryBuilderTest

--- is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/course/type/CourseTypeQueryBuilderTest.java
+++ is-lnu-persistence/src/test/java/org/lnu/is/dao/builder/course/type/CourseTypeQueryBuilderTest.java
@@ -2,10 +2,15 @@ package org.lnu.is.dao.builder.course.type;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.lnu.is.domain.coursetype.CourseType;
 import org.lnu.is.pagination.MultiplePagedSearch;
+import org.lnu.is.pagination.OrderBy;
+import org.lnu.is.pagination.OrderByType;
 
 public class CourseTypeQueryBuilderTest {
 
@@ -37,6 +42,27 @@ public class CourseTypeQueryBuilderTest {
 	}
 	
 	@Test
+	public void testBuildWithOrderBy() throws Exception {
+		// Given
+		CourseType context = new CourseType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expected = "SELECT e FROM CourseType e WHERE e.status=:status AND e.crtUserGroup IN (:userGroups) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
 	public void testBuildWithDisabledSecurityConstaint() throws Exception {
 		// Given
 		unit.setSecurity(false);
@@ -46,6 +72,29 @@ public class CourseTypeQueryBuilderTest {
 		String expected = "SELECT e FROM CourseType e WHERE e.status=:status ";
 		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledSecurityConstaintWithOrderBy() throws Exception {
+		// Given
+		unit.setSecurity(false);
+		
+		CourseType context = new CourseType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expected = "SELECT e FROM CourseType e WHERE e.status=:status ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -73,6 +122,29 @@ public class CourseTypeQueryBuilderTest {
 	}
 	
 	@Test
+	public void testBuildWithDisabledStatusCOnstsintWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		
+		CourseType context = new CourseType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expected = "SELECT e FROM CourseType e WHERE e.crtUserGroup IN (:userGroups) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
 	public void testBuildWithDisabledDefaultConstraint() throws Exception {
 		// Given
 		unit.setActive(false);
@@ -83,6 +155,30 @@ public class CourseTypeQueryBuilderTest {
 		String expected = "SELECT e FROM CourseType e ";
 		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithDisabledDefaultConstraintWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		CourseType context = new CourseType();
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expected = "SELECT e FROM CourseType e ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
@@ -112,7 +208,32 @@ public class CourseTypeQueryBuilderTest {
 	}
 	
 	@Test
-	public void testBuildWithParametersAndDisabledDEfaultConstraint() throws Exception {
+	public void testBuildWithParametersWithOrderBy() throws Exception {
+		// Given
+		String abbrName = "abbr name";
+		String name = "name";
+		CourseType context = new CourseType();
+		context.setName(name);
+		context.setAbbrName(abbrName);
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+
+		String expected = "SELECT e FROM CourseType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') ) AND e.status=:status AND e.crtUserGroup IN (:userGroups) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithParametersAndDisabledDefaultConstraint() throws Exception {
 		// Given
 		unit.setActive(false);
 		unit.setSecurity(false);
@@ -126,6 +247,34 @@ public class CourseTypeQueryBuilderTest {
 		String expected = "SELECT e FROM CourseType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') ) ";
 		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
 		pagedSearch.setEntity(context);
+		
+		// When
+		String actualQuery = unit.build(pagedSearch);
+		
+		// Then
+		assertEquals(expected, actualQuery);
+	}
+	
+	@Test
+	public void testBuildWithParametersAndDisabledDefaultConstraintWithOrderBy() throws Exception {
+		// Given
+		unit.setActive(false);
+		unit.setSecurity(false);
+		
+		String abbrName = "abbr name";
+		String name = "name";
+		CourseType context = new CourseType();
+		context.setName(name);
+		context.setAbbrName(abbrName);
+		
+		OrderBy orderBy1 = new OrderBy("abbrName", OrderByType.ASC);
+		OrderBy orderBy2 = new OrderBy("name", OrderByType.DESC);
+		List<OrderBy> orders = Arrays.asList(orderBy1, orderBy2);
+		
+		String expected = "SELECT e FROM CourseType e WHERE ( e.name LIKE CONCAT('%',:name,'%') OR e.abbrName LIKE CONCAT('%',:abbrName,'%') ) ORDER BY e.abbrName ASC, e.name DESC";
+		MultiplePagedSearch<CourseType> pagedSearch = new MultiplePagedSearch<>();
+		pagedSearch.setEntity(context);
+		pagedSearch.setOrders(orders);
 		
 		// When
 		String actualQuery = unit.build(pagedSearch);
