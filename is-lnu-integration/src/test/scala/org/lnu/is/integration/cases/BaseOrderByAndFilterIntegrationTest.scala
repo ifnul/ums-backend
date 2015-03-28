b074a9ffcbc6695371d355422816dc89baa99e91
Oleh Zanevych <Oleh.Zanevych@gmail.com>
2015-03-29 01:27:10 +0200

    Removed unnecessary inmporty imports

--- is-lnu-integration/src/test/scala/org/lnu/is/integration/cases/BaseOrderByAndFilterIntegrationTest.scala
+++ is-lnu-integration/src/test/scala/org/lnu/is/integration/cases/BaseOrderByAndFilterIntegrationTest.scala
@@ -1,20 +1,12 @@
 package org.lnu.is.integration.cases
 
-import scala.concurrent.duration.DurationInt
 import io.gatling.core.Predef.checkBuilder2Check
 import io.gatling.core.Predef.findCheckBuilder2ValidatorCheckBuilder
 import io.gatling.core.Predef.exec
-import io.gatling.core.Predef.stringToExpression
-import io.gatling.core.Predef.validatorCheckBuilder2CheckBuilder
 import io.gatling.core.Predef.value2Expression
-import io.gatling.core.Predef.value2Success
-import io.gatling.http.Predef.ELFileBody
 import io.gatling.http.Predef.http
-import io.gatling.http.Predef.jsonPath
 import io.gatling.http.Predef.status
 import io.gatling.core.structure.ChainBuilder
-import scala.collection.mutable.ArrayBuffer
-import io.gatling.http.request.builder.HttpRequestBuilder
 
 /**
  * @author OlehZanevych
@@ -31,7 +23,7 @@ abstract class BaseOrderByAndFilterIntegrationTest {
       val url = method._2 + "?orderBy=" + method._3.map{str => str._1 + "-asc"}.mkString(",") + method._3.map{str => str._1 + "=" + str._2}.mkString("&", "&", "")
       result = result.exec(checkUrl(method._1, url))
     }}
-    return result;
+    return result
     
   }
   
