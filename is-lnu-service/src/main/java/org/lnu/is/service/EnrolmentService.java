da557f01c3b4dafc013400efdc2446c69f3fe25b
Ivan Ursul <Ivanon2@gmail.com>
2016-03-17 23:26:53 +0200

    Attached specofferWave to enrolment service

--- is-lnu-service/src/main/java/org/lnu/is/service/EnrolmentService.java
+++ is-lnu-service/src/main/java/org/lnu/is/service/EnrolmentService.java
@@ -1,6 +1,7 @@
 package org.lnu.is.service;
 
 import org.lnu.is.dao.dao.enrolment.EnrolmentDao;
+import org.lnu.is.dao.dao.specoffer.SpecOfferWaveDao;
 import org.lnu.is.domain.enrolment.Enrolment;
 import org.lnu.is.domain.enrolment.EnrolmentRating;
 
@@ -8,9 +9,15 @@ import java.util.List;
 
 public class EnrolmentService extends DefaultService<Enrolment, Enrolment, Long, EnrolmentDao> implements Service<Enrolment, Enrolment, Long> {
 
+    private SpecOfferWaveDao specOfferWaveDao;
+
     public List<EnrolmentRating> getRatingEnrolments(long specofferId) {
         // todo: count limit for цільовий вступ
         // todo: count limit for першочерговий вступ
         return dao.getRatingEnrolments(specofferId);
+    }
+
+    public void setSpecOfferWaveDao(SpecOfferWaveDao specOfferWaveDao) {
+        this.specOfferWaveDao = specOfferWaveDao;
     }
 }
