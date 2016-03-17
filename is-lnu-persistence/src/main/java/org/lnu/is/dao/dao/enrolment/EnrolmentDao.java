3b2b6ad5f625cd424b2ef184ce4570d7180a0222
Ivan Ursul <Ivanon2@gmail.com>
2016-03-17 23:05:31 +0200

    Fix bug with resource queries

--- is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/enrolment/EnrolmentDao.java
+++ is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/enrolment/EnrolmentDao.java
@@ -1,5 +1,7 @@
 package org.lnu.is.dao.dao.enrolment;
 
+import com.lambdista.util.Try;
+import org.apache.commons.io.IOUtils;
 import org.lnu.is.dao.dao.DefaultDao;
 import org.lnu.is.domain.enrolment.Enrolment;
 import org.lnu.is.domain.enrolment.EnrolmentRating;
@@ -14,133 +16,11 @@ import java.util.stream.Collectors;
 public class EnrolmentDao extends DefaultDao<Enrolment, Enrolment, Long> {
     private static final String SPECOFFER_ID_PLACEHOLDER = "$$SPECOFFER_ID$$";
 
-    private static final String QUERY_SQL = "SELECT " +
-            "rating.enrolment_id,\n" +
-            "rating.kb,\n" +
-            "rating.priority, \n" +
-            "e.person_id, \n" +
-            "e.docseries, \n" +
-            "e.docnum, \n" +
-            "e.doctext, \n" +
-            "e.isstate, \n" +
-            "e.iscontract, \n" +
-            "e.specoffer_id, \n" +
-            "e.department_id, \n" +
-            "e.personpaper_id, \n" +
-            "e.mark, \n" +
-            "e.isprivilege, \n" +
-            "e.ishostel, \n" +
-            "e.enrolmenttype_id, \n" +
-            "e.uid, \n" +
-            "e.utid, \n" +
-            "e.uapp, \n" +
-            "e.markscale_id, \n" +
-            "e.isedustate, \n" +
-            "e.isinterview, \n" +
-            "e.isoriginal " +
-            "FROM (\n" +
-            "    SELECT \n" +
-            "        mon.enrolment_id,\n" +
-            "        mon.kb,\n" +
-            "        mon.priority\n" +
-            "    FROM v_enrolment_mon mon\n" +
-            "    WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "    \n" +
-            "    UNION\n" +
-            "    \n" +
-            "    SELECT \n" +
-            "        eda.enrolment_id,\n" +
-            "        eda.kb,\n" +
-            "        eda.priority\n" +
-            "    FROM v_enrolment_destination_admission eda\n" +
-            "    WHERE \n" +
-            "    eda.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "    AND eda.enrolment_id NOT IN (\n" +
-            "        SELECT \n" +
-            "            mon.enrolment_id \n" +
-            "        FROM v_enrolment_mon mon\n" +
-            "        WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "        )\n" +
-            "    UNION\n" +
-            "    SELECT \n" +
-            "        eoce.enrolment_id,\n" +
-            "        eoce.kb,\n" +
-            "        eoce.priority\n" +
-            "    FROM v_enrolment_off_competition_entry eoce\n" +
-            "    WHERE\n" +
-            "    eoce.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + " \n" +
-            "    AND eoce.enrolment_id NOT IN (\n" +
-            "\n" +
-            "        SELECT \n" +
-            "            mon.enrolment_id  \n" +
-            "        FROM v_enrolment_mon mon\n" +
-            "        WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "        UNION\n" +
-            "        SELECT \n" +
-            "            eda.enrolment_id \n" +
-            "        FROM v_enrolment_destination_admission eda\n" +
-            "        WHERE eda.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "        AND eda.enrolment_id NOT IN (\n" +
-            "            SELECT \n" +
-            "                mon.enrolment_id \n" +
-            "            FROM v_enrolment_mon mon\n" +
-            "            WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "        )\n" +
-            "        \n" +
-            "    )\n" +
-            "    UNION\n" +
-            "    SELECT \n" +
-            "        default_entry.enrolment_id,\n" +
-            "        default_entry.kb,\n" +
-            "        default_entry.priority\n" +
-            "    FROM v_enrolment_default_entry default_entry\n" +
-            "    WHERE default_entry.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + " \n" +
-            "    AND default_entry.enrolment_id NOT IN (\n" +
-            "        SELECT \n" +
-            "            mon.enrolment_id \n" +
-            "        FROM v_enrolment_mon mon\n" +
-            "        WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "        UNION\n" +
-            "        SELECT \n" +
-            "            eda.enrolment_id \n" +
-            "        FROM v_enrolment_destination_admission eda\n" +
-            "        WHERE eda.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + " \n" +
-            "        AND eda.enrolment_id NOT IN (\n" +
-            "            SELECT \n" +
-            "                mon.enrolment_id \n" +
-            "            FROM v_enrolment_mon mon\n" +
-            "            WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "            )\n" +
-            "        UNION\n" +
-            "        SELECT \n" +
-            "            eoce.enrolment_id \n" +
-            "        FROM v_enrolment_off_competition_entry eoce\n" +
-            "        WHERE eoce.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + " \n" +
-            "        AND eoce.enrolment_id NOT IN (\n" +
-            "            SELECT \n" +
-            "                mon.enrolment_id  \n" +
-            "            FROM v_enrolment_mon mon\n" +
-            "            WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "            UNION\n" +
-            "            SELECT \n" +
-            "                eda.enrolment_id \n" +
-            "            FROM v_enrolment_destination_admission eda\n" +
-            "            WHERE eda.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + " \n" +
-            "            AND eda.enrolment_id NOT IN (\n" +
-            "                SELECT \n" +
-            "                    mon.enrolment_id \n" +
-            "                FROM v_enrolment_mon mon\n" +
-            "                WHERE mon.specoffer_id = "  + SPECOFFER_ID_PLACEHOLDER + "\n" +
-            "            )\n" +
-            "            \n" +
-            "        )\n" +
-            "    )\n" +
-            "    ORDER BY priority DESC,kb\n" +
-            ") AS rating\n" +
-            "INNER JOIN q_dc_enrolment e ON e.id = rating.enrolment_id\n";
-
     public List<EnrolmentRating> getRatingEnrolments(long specofferId) {
-        String sql = QUERY_SQL.replace(SPECOFFER_ID_PLACEHOLDER, String.valueOf(specofferId));
+        String queryTemplate = Try.apply(() ->
+                IOUtils.toString(this.getClass().getResourceAsStream("/queries/enrolment_rating.sql"), "UTF-8")).get();
+        String sql = queryTemplate.replace(SPECOFFER_ID_PLACEHOLDER, String.valueOf(specofferId));
+
         List<Object[]> enrolmentRatings = getPersistenceManager().executeNativeQuery(sql);
 
         return enrolmentRatings.stream()
---
+++ is-lnu-persistence/src/main/resources/queries/enrolment_rating.sql
@@ -0,0 +1,122 @@
+SELECT rating.enrolment_id,
+rating.kb,
+rating.priority,
+e.person_id,
+e.docseries,
+e.docnum,
+e.doctext,
+e.isstate,
+e.iscontract,
+e.specoffer_id,
+e.department_id,
+e.personpaper_id,
+e.mark,
+e.isprivilege,
+e.ishostel,
+e.enrolmenttype_id,
+e.uid,
+e.utid,
+e.uapp,
+e.markscale_id,
+e.isedustate,
+e.isinterview,
+e.isoriginal FROM (
+    SELECT
+        mon.enrolment_id,
+        mon.kb,
+        mon.priority
+    FROM v_enrolment_mon mon
+    WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+
+    UNION
+
+    SELECT
+        eda.enrolment_id,
+        eda.kb,
+        eda.priority
+    FROM v_enrolment_destination_admission eda
+    WHERE
+    eda.specoffer_id = $$SPECOFFER_ID$$
+    AND eda.enrolment_id NOT IN (
+        SELECT
+            mon.enrolment_id
+        FROM v_enrolment_mon mon
+        WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+        )
+    UNION
+    SELECT
+        eoce.enrolment_id,
+        eoce.kb,
+        eoce.priority
+    FROM v_enrolment_off_competition_entry eoce
+    WHERE
+    eoce.specoffer_id = $$SPECOFFER_ID$$
+    AND eoce.enrolment_id NOT IN (
+
+        SELECT
+            mon.enrolment_id
+        FROM v_enrolment_mon mon
+        WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+        UNION
+        SELECT
+            eda.enrolment_id
+        FROM v_enrolment_destination_admission eda
+        WHERE eda.specoffer_id = $$SPECOFFER_ID$$
+        AND eda.enrolment_id NOT IN (
+            SELECT
+                mon.enrolment_id
+            FROM v_enrolment_mon mon
+            WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+        )
+
+    )
+    UNION
+    SELECT
+        default_entry.enrolment_id,
+        default_entry.kb,
+        default_entry.priority
+    FROM v_enrolment_default_entry default_entry
+    WHERE default_entry.specoffer_id = $$SPECOFFER_ID$$
+    AND default_entry.enrolment_id NOT IN (
+        SELECT
+            mon.enrolment_id
+        FROM v_enrolment_mon mon
+        WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+        UNION
+        SELECT
+            eda.enrolment_id
+        FROM v_enrolment_destination_admission eda
+        WHERE eda.specoffer_id = $$SPECOFFER_ID$$
+        AND eda.enrolment_id NOT IN (
+            SELECT
+                mon.enrolment_id
+            FROM v_enrolment_mon mon
+            WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+            )
+        UNION
+        SELECT
+            eoce.enrolment_id
+        FROM v_enrolment_off_competition_entry eoce
+        WHERE eoce.specoffer_id = $$SPECOFFER_ID$$
+        AND eoce.enrolment_id NOT IN (
+            SELECT
+                mon.enrolment_id
+            FROM v_enrolment_mon mon
+            WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+            UNION
+            SELECT
+                eda.enrolment_id
+            FROM v_enrolment_destination_admission eda
+            WHERE eda.specoffer_id = $$SPECOFFER_ID$$
+            AND eda.enrolment_id NOT IN (
+                SELECT
+                    mon.enrolment_id
+                FROM v_enrolment_mon mon
+                WHERE mon.specoffer_id = $$SPECOFFER_ID$$
+            )
+
+        )
+    )
+    ORDER BY priority DESC,kb
+) AS rating
+INNER JOIN q_dc_enrolment e ON e.id = rating.enrolment_id
