15e3f3f15c9d99c5716ef2150e4e058d5f464623
Ivan Ursul <Ivanon2@gmail.com>
2016-03-17 23:34:51 +0200

    Added logic for getting specofferWave

--- is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/specoffer/SpecOfferWaveDao.java
+++ is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/specoffer/SpecOfferWaveDao.java
@@ -1,8 +1,20 @@
 package org.lnu.is.dao.dao.specoffer;
 
 import org.lnu.is.dao.dao.DefaultDao;
+import org.lnu.is.domain.specoffer.SpecOffer;
 import org.lnu.is.domain.specoffer.SpecOfferWave;
+import org.lnu.is.queries.Query;
+
+import java.util.Collections;
+import java.util.Map;
 
 public class SpecOfferWaveDao extends DefaultDao<SpecOfferWave, SpecOfferWave, Long> {
 
+    public SpecOfferWave getLastSpecOfferWave(SpecOffer specOffer) {
+        Map<String, Object> params = Collections.singletonMap("specOffer", specOffer);
+        String sql = "SELECT sw FROM SpecOfferWave sw " +
+                "WHERE sw.id = (SELECT isw.id FROM SpecOfferWave isw WHERE isw.specOffer = :specOffer)";
+        Query<SpecOfferWave> query = new Query<>(SpecOfferWave.class, sql, params);
+        return getSingleResult(query);
+    }
 }
--- is-lnu-service/src/main/java/org/lnu/is/service/EnrolmentService.java
+++ is-lnu-service/src/main/java/org/lnu/is/service/EnrolmentService.java
@@ -1,17 +1,23 @@
 package org.lnu.is.service;
 
+import org.lnu.is.dao.dao.Dao;
 import org.lnu.is.dao.dao.enrolment.EnrolmentDao;
 import org.lnu.is.dao.dao.specoffer.SpecOfferWaveDao;
 import org.lnu.is.domain.enrolment.Enrolment;
 import org.lnu.is.domain.enrolment.EnrolmentRating;
+import org.lnu.is.domain.specoffer.SpecOffer;
+import org.lnu.is.domain.specoffer.SpecOfferWave;
 
 import java.util.List;
 
 public class EnrolmentService extends DefaultService<Enrolment, Enrolment, Long, EnrolmentDao> implements Service<Enrolment, Enrolment, Long> {
 
+    private Dao<SpecOffer, SpecOffer, Long> specOfferDao;
     private SpecOfferWaveDao specOfferWaveDao;
 
     public List<EnrolmentRating> getRatingEnrolments(long specofferId) {
+        SpecOffer specOffer = specOfferDao.getByUid(specofferId);
+        SpecOfferWave specOfferWave = specOfferWaveDao.getLastSpecOfferWave(specOffer);
         // todo: count limit for цільовий вступ
         // todo: count limit for першочерговий вступ
         return dao.getRatingEnrolments(specofferId);
@@ -19,5 +25,9 @@ public class EnrolmentService extends DefaultService<Enrolment, Enrolment, Long,
 
     public void setSpecOfferWaveDao(SpecOfferWaveDao specOfferWaveDao) {
         this.specOfferWaveDao = specOfferWaveDao;
+    }
+
+    public void setSpecOfferDao(Dao<SpecOffer, SpecOffer, Long> specOfferDao) {
+        this.specOfferDao = specOfferDao;
     }
 }
