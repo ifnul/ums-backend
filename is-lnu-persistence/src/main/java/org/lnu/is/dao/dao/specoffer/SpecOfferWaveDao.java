a8dae007598d9800ae5cefcd52487610768ff311
Ivan Ursul <Ivanon2@gmail.com>
2016-03-17 23:23:10 +0200

    Added SpecOfferWaveDao

---
+++ is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/specoffer/SpecOfferWaveDao.java
@@ -0,0 +1,8 @@
+package org.lnu.is.dao.dao.specoffer;
+
+import org.lnu.is.dao.dao.DefaultDao;
+import org.lnu.is.domain.specoffer.SpecOfferWave;
+
+public class SpecOfferWaveDao extends DefaultDao<SpecOfferWave, SpecOfferWave, Long> {
+
+}
