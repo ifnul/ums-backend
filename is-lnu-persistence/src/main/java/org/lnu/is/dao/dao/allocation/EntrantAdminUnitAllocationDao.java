6402a2c4cf8f41d506007bd87725f552bc0f3e81
Ivan Ursul <Ivanon2@gmail.com>
2016-03-18 16:56:26 +0200

    Added admin unit proper allocation

--- is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/allocation/EntrantAdminUnitAllocationDao.java
+++ is-lnu-persistence/src/main/java/org/lnu/is/dao/dao/allocation/EntrantAdminUnitAllocationDao.java
@@ -1,5 +1,7 @@
 package org.lnu.is.dao.dao.allocation;
 
+import com.lambdista.util.Try;
+import org.apache.commons.io.IOUtils;
 import org.lnu.is.domain.admin.unit.AdminUnitEntrantAllocation;
 import org.springframework.stereotype.Repository;
 
@@ -15,38 +17,20 @@ import static java.util.stream.Collectors.toList;
  */
 @Repository
 public class EntrantAdminUnitAllocationDao {
-    private static final String QUERY_SQL = "SELECT \n" +
-            "COUNT(enrolment.id) AS entrantCount, \n" +
-            "au.name AS adminUnit,\n" +
-            "au.id AS adminUnitId\n" +
-            "FROM q_dc_enrolment enrolment\n" +
-            "INNER JOIN q_ob_person person ON  person.id = enrolment.person_id\n" +
-            "INNER JOIN q_od_personaddress person_address ON person_address.id = \n" +
-            "(SELECT MAX(id) FROM q_od_personaddress pa WHERE pa.person_id = enrolment.person_id GROUP BY pa.person_id)\n" +
-            "INNER JOIN (\n" +
-            "  WITH RECURSIVE recursetree(id, parent_id, path, name) AS (\n" +
-            "    SELECT \n" +
-            "        id, \n" +
-            "        parent_id, \n" +
-            "        array[id] AS path, \n" +
-            "        name \n" +
-            "    FROM q_ob_adminunit WHERE parent_id = %d \n" +
-            "  UNION ALL\n" +
-            "    SELECT t.id, t.parent_id, rt.path || t.id, t.name\n" +
-            "    FROM q_ob_adminunit t\n" +
-            "    JOIN recursetree rt ON rt.id = t.parent_id\n" +
-            "  )\n" +
-            "  SELECT path[1] AS admin_unit_id, id, name FROM recursetree ORDER BY path\n" +
-            ") AS admin_unit_root ON admin_unit_root.id = person_address.adminunit_id\n" +
-            "INNER JOIN q_ob_adminunit au ON au.id = admin_unit_root.admin_unit_id\n" +
-            "GROUP BY au.name, au.id\n" +
-            "\n";
+    private static final String TIME_PERIOD_ID_PLACEHOLDER = "$$TIME_PERIOD_ID$$";
+    private static final String MAIN_ADMIN_UNIT_ID_PLACEHOLDER = "$$MAIN_ADMIN_UNIT_ID$$";
 
     @PersistenceContext
     private EntityManager entityManager;
 
     public List<AdminUnitEntrantAllocation> getAdminUnitEntrantsAllocation(long mainAdminUnitId, long timePeriodId) {
-        Query query = entityManager.createNativeQuery(String.format(QUERY_SQL, mainAdminUnitId));
+        String queryTemplate = Try.apply(() ->
+                IOUtils.toString(this.getClass().getResourceAsStream("/queries/admin_unit_allocation.sql"), "UTF-8")).get();
+        String sql = queryTemplate
+                .replace(TIME_PERIOD_ID_PLACEHOLDER, String.valueOf(timePeriodId))
+                .replace(MAIN_ADMIN_UNIT_ID_PLACEHOLDER, String.valueOf(mainAdminUnitId));
+
+        Query query = entityManager.createNativeQuery(sql);
         List<Object[]> resultList = query.getResultList();
         return resultList.stream()
                 .map(arr -> new AdminUnitEntrantAllocation(String.valueOf(arr[1]), Long.valueOf(arr[2].toString()), Integer.valueOf(arr[0].toString())))
