b071f81d0eab251e95d02a08fade267fbcc42e1d
illay1994 <i_mazyriak@lnu.edu.ua>
2016-03-26 17:51:11 +0200

    Update SpecOffetTypeController.java

--- is-lnu-rest-api/src/main/java/org/lnu/is/web/rest/controller/specoffer/type/SpecOffetTypeController.java
+++ is-lnu-rest-api/src/main/java/org/lnu/is/web/rest/controller/specoffer/type/SpecOffetTypeController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 /**
